9cbd81ae202847cef741410640db29600eb4c610
Joao Victor Poloniato Buss <150697033+JoaoVictorPoloniato@users.noreply.github.com>
2024-03-25 10:08:39 -0400

    Mudado a imagem para seleção
    mudado a imagem e o menu inicial

--- my-portfolio/src/components/Photo/Photo.jsx
+++ my-portfolio/src/components/Photo/Photo.jsx
@@ -1,28 +1,14 @@
 import React from "react";
+import myPhoto from "./myphoto.png";
 import './index.css';
 
 function Photo() {
-    const handleMouseMove = (e) => {
-        const photo = e.target;
-        const boundingBox = photo.getBoundingClientRect();
-        const offsetX = (e.clientX - boundingBox.left) / boundingBox.width - 0.5;
-        const offsetY = (e.clientY - boundingBox.top) / boundingBox.height - 0.5;
-
-        photo.style.transform = `translate(${offsetX * 10}px, ${offsetY * 10}px)`;
-    };
-
-    const handleMouseLeave = (e) => {
-        e.target.style.transform = 'none';
-    };
-
     return (
         <div className="photo-container">
             <img
                 className="photo"
-                src="https://avatars.githubusercontent.com/u/117031491?v=4"
+                src={myPhoto}
                 alt="eu"
-                onMouseMove={handleMouseMove}
-                onMouseLeave={handleMouseLeave}
             />
         </div>
     );
