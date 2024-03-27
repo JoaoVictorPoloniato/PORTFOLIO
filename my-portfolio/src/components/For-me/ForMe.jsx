537e9caa59a584a0b3f15f6adc526b9555b68e83
Joao Victor Poloniato Buss <150697033+JoaoVictorPoloniato@users.noreply.github.com>
2024-03-27 08:31:36 -0400

    Trocado meu texto de apresentação

--- my-portfolio/src/components/For-me/ForMe.jsx
+++ my-portfolio/src/components/For-me/ForMe.jsx
@@ -4,9 +4,9 @@ import './index.css';
 function ForMe() {
     return (
         <div className="form-container">
-            <p>É um prazer apresentar meu portfólio como desenvolvedor front-End com conhecimento em HTML, CSS, JavaScript, React.js, Angular, Node.js, TypeScript, Tailwind CSS, Next.js, Bootstrap.</p>
+            <p>É um prazer apresentar meu portfólio como desenvolvedor front-End com conhecimento em HTML, CSS, JavaScript, React.js, Node.js, TypeScript, PHP, Tailwind CSS, Next.js, Bootstrap, Python e C# .NET.</p>
             <p>Sou um apaixonado por tecnologia e design de interfaces. Desde o início da minha transição de carreira, tenho me dedicado a aprimorar minhas habilidades como desenvolvedor front-end e criar experiências web memoráveis.</p>
-            <p>Nesta apresentação, vou compartilhar minha jornada e mostrar como essas habilidades me capacitam a criar interfaces web incríveis e funcionais. Vamos mergulhar no mundo do desenvolvimento front-end!</p>
+            <p>Nesta minha apresentação, vou compartilhar alguns projetos e mostrar minhas habilidades. E deixar meus contatos, para caso queiram conversar comigo, e compartilhar algo, ou pedir algo. 😁😉 </p>
 
         </div>
     );
