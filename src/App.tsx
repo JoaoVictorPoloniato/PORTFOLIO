9a8f586bd6b2620116385479b545f493d40a0a31
JoaoJoaoVictorPoloniato JoaoVictorPoloniato Victor <polonito155@outlook.com>
2026-02-17 13:52:55 -0400

    fix: add vite config

--- src/App.tsx
+++ src/App.tsx
@@ -15,7 +15,7 @@ const App = () => (
       <TooltipProvider>
         <Toaster />
         <Sonner />
-        <BrowserRouter basename="/PORTFOLIO"> {/* ADICIONE ESTA LINHA */}
+        <BrowserRouter>
           <Routes>
             <Route path="/" element={<Index />} />
             <Route path="*" element={<NotFound />} />
